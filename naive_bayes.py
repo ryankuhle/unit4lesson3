087fe26efafca80be803eb084cd7827c010d9240
Ryan Kuhle <ryan@ryankuhle.com>
2015-12-29 12:21:43 -0600

    Plot histogram of average weight against ideal weight

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import matplotlib.pylab as plt
+
+show_weight_dist = 'Y' # Show weight distribution plot, Y or N
 
 df = pd.read_csv("ideal_weight.csv")
 df.columns = ['id', 'sex', 'actual', 'ideal', 'diff']
@@ -6,13 +9,15 @@ df.columns = ['id', 'sex', 'actual', 'ideal', 'diff']
 df['sex'] = df['sex'].map(lambda x: x.lstrip('\'').rstrip('\''))
 del df['id']
 
+def weight_distribution():
+    '''
+    weight_distribution
+    Plot histogram of actual weight versus ideal weight.
+    '''
+    plt.hist(df['actual'], alpha=0.5, label="Actual Weight")
+    plt.hist(df['ideal'], alpha=0.5, label="Ideal Weight")
+    plt.legend(loc='upper right')
+    plt.show()
 
-
-
-
-
-
-
-print df
-
-#Plot the distributions of actual weight and ideal weight.
+if show_weight_dist == 'Y':
+    weight_distribution()
